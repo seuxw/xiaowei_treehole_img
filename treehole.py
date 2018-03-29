92c3db93f00cb733ba7db722e15f13be762b649b
TauWoo <tauwoo@seuxw.cc>
2018-03-29 19:13:31 +0800

    图片合成的小调整

--- treehole.py
+++ treehole.py
@@ -6,10 +6,10 @@ from util.img.img_selector import ImgSelector
 if __name__ == "__main__":
     contents = {
         'title':'小微树洞·悄悄话',
-        'content_send':'\t\t小微你好，我是巴拉巴拉，我想要拉吧拉吧。请问我现在该怎么办呢？',
+        'content_send':'小微你好，我本来喜欢一个女生。但那个女生的性格特别的man，一点都不温柔可爱，也不打算找男朋友。我该怎么办？',
         'sender':'测试人员',
         'date_send':'2018-01-02',
-        'content_reply':"测试数据1213测试数据241231数据测试",
+        'content_reply':"你可以考虑和她当哥们呀～",
         'replier':"测试人员2",
         'date_reply':'2018-03-21'
     }
--- util/img/img_blender.py
+++ util/img/img_blender.py
@@ -4,15 +4,14 @@ from PIL import Image, ImageFont, ImageDraw
 from io import BytesIO
 
 def set_paragraph(text, count):
+    text = "\t\t"+text
     rtn_text = str()
-    print(len(text), count)
     for idx in range(0, int(len(text)/count)+1):
         try:
             rtn_text += text[idx*(count):(idx+1)*count]
             rtn_text += '\n'
         except Exception:
             rtn_text += text[idx*(count-1):]
-    print(rtn_text)
     return rtn_text
 
 class ImgBlender():
@@ -35,8 +34,8 @@ class ImgBlender():
     @property
     def __set_default_config__(self):
         # 文本换行设置
-        self.contents['content_send'] = set_paragraph(self.contents['content_send'], 16)
-        self.contents['content_reply'] = set_paragraph(self.contents['content_reply'], 16)
+        self.contents['content_send'] = set_paragraph(self.contents['content_send'], 15)
+        self.contents['content_reply'] = set_paragraph(self.contents['content_reply'], 15)
 
         # 字体字典配置
         self.font_dict["title"] = ImageFont.truetype(font='./bin/ttf/weibeijian.ttf', size=100, encoding="utf-8")
@@ -44,16 +43,16 @@ class ImgBlender():
         self.font_dict["sender"] = self.font_dict["replier"] = self.font_dict["date_send"] = self.font_dict["date_reply"] = ImageFont.truetype(font='./bin/ttf/xihei.ttf', size=60, encoding='utf-8')
 
         # 文字位置配置
-        self.posi_dict["title"] = (100,100)
-        self.posi_dict["content_send"] = (100,270)
+        self.posi_dict["title"] = (150,100)
+        self.posi_dict["content_send"] = (150,270)
         self.posi_dict["sender"] = (950,700)
         self.posi_dict["date_send"] = (950,800)
-        self.posi_dict["content_reply"] = (100,1000)
-        self.posi_dict["replier"] = (950,1800)
-        self.posi_dict["date_reply"] = (950,1900)
+        self.posi_dict["content_reply"] = (150,1000)
+        self.posi_dict["replier"] = (950,2000)
+        self.posi_dict["date_reply"] = (950,2100)
 
         # 文字颜色配置
-        self.fill_dict["title"] = (0,0,0)
+        self.fill_dict["title"] = "#ffffff"
         self.fill_dict["content_send"] = (80,80,80)
         self.fill_dict["sender"] = (80,80,80)
         self.fill_dict["date_send"] = (80,80,80)
